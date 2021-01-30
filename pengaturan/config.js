a498edffaaec0db478332e6e3434327eb528083a
TestBotWa <78215290+TestBotWa@users.noreply.github.com>
2021-01-30 20:10:26 +0700

    Update config.js

--- pengaturan/config.js
+++ pengaturan/config.js
@@ -1,6 +1,6 @@
 const config = {
-        botName: 'JINGAN BOT',
-        ownerName: 'Muhammad Iqbal',
+        botName: 'ITACHI BOT',
+        ownerName: 'RIO_GANS',
         youtube: 'https://m.youtube.com/channel/UCb6qlya4nhSYqNBdSQVes-A',
         instagram: 'https://instagram.com/_miqbal.22',
 }
